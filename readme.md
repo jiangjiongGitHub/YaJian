a5f888c911c59df9de6cb00c314e62c235e114b8
gg <gg@SKY-20181018UXU>
2019-04-08 14:26:20 +0800

    可用tomcat发布，修改路径为相对路径，绝对路径不包括项目名，需要手动拼接。

--- readme.md
+++ readme.md
@@ -1,6 +1,12 @@
 # YaJian
 
----
+
+### Tomcat 9
+0. add
+0. commit
+0. push
+0. pull
+
 
 ### git命令
 0. add
--- src/main/java/YaJian/SpringBoot.java
+++ src/main/java/YaJian/SpringBoot.java
@@ -2,9 +2,17 @@ package YaJian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBoot {
+public class SpringBoot extends SpringBootServletInitializer {
+
+	/** 屏蔽自带tomcat **/
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBoot.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot.class, args);
