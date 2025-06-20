1e118557080fb8fccead5c429d5fab82c3cb25d8
jiongjiang <1322245688@qq.com>
2025-06-20 11:05:31 +0800

    升级springboot版本

--- src/main/java/YaJian/SpringBoot.java
+++ src/main/java/YaJian/SpringBoot.java
@@ -2,20 +2,12 @@ package YaJian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBoot extends SpringBootServletInitializer {
+public class SpringBoot {
 
-	/** 屏蔽自带tomcat **/
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SpringBoot.class);
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot.class, args);
+    }
 
 }
