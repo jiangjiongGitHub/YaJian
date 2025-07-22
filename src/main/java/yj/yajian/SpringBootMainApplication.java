8647d3f820d503e496fdd1a702ad12702d0eb516
jiangjiong <1322245688@qq.com>
2025-07-22 16:58:16 +0800

    日志调整为logbak

--- src/main/java/yj/yajian/SpringBootMainApplication.java
+++ src/main/java/yj/yajian/SpringBootMainApplication.java
@@ -5,12 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.io.File;
+
 @Slf4j
 @SpringBootApplication
 @EnableScheduling
 public class SpringBootMainApplication {
 
     public static void main(String[] args) {
+        new File("./logs").mkdirs();
+        log.info("SpringBootMainApplication starting");
+
         SpringApplication.run(SpringBootMainApplication.class, args);
         log.info("http://127.0.0.1:18888/main.html");
     }
